570eb7da2d89c9d212ba59277b1a84c5f2c60a80
Avital Piven <avitalp@protonmail.com>
2021-07-27 22:39:03 +0300

    ci: 🚀 deploy build script

--- Makefile
+++ Makefile
@@ -2,4 +2,9 @@ build-lambda:
 	mkdir -p functions
 	$(MAKE) -C backend download build OUTPUT="$(PWD)/functions/kurzr"
 
-build: build-lambda
+build-frontend:
+	npm run --prefix frontend/ build
+	rm -r site
+	mv frontend/out site
+
+build: build-lambda build-frontend
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,19 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*", // Proxy to Backend
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfig,
+      async rewrites() {
+        return [
+          {
+            source: "/api/:path*",
+            destination: "http://localhost:4000/api/:path*", // Proxy to Backend
+          },
+        ]
       },
-    ]
-  },
+    }
+  }
+
+  return defaultConfig
 }
